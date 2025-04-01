0594badd96cbfa190e17c7794fecbb114524a1b3
nav-github3 <navneetlonare77@gmail.com>
2025-04-01 09:03:56 +0530

    git add some

--- 02-click-event/src/Button.jsx
+++ 02-click-event/src/Button.jsx
@@ -1,30 +1,50 @@
+// function Button() {
+
+// 	// const onClickHandler = () =>  console.log("navneet !! ")
+// 	// const onClickHandler2 = (name) => console.log(`${name} stop clicking me `)
+
+// 	let count = 0; 
+// 	const handleClick = (name) => {
+
+// 		if(count < 3){
+// 			count++;
+// 			console.log(`${name} ypu click me ${count}times `)
+// 		}
+// 		else {
+// 			console.log(`${name} stop clicking me !! `)
+// 		}
+// 	}
+
+
+// 	// event object = An object that contains information about the event that occured. It is passed as an argument to the event handler function.
+// 	const handleClick1 = (e) => {
+// 			console.log(e);
+// 	}
+
+
+// 	return( <button onClick={(e) => {handleClick1(e)}} >Click me</button> )
+
+// }
+// export default Button
+
+
+
+
+
 function Button() {
+	 const handleClick =  (name)  => console.log(`${name} you clicked me `);
 
-	// const onClickHandler = () =>  console.log("navneet !! ")
-	// const onClickHandler2 = (name) => console.log(`${name} stop clicking me `)
-
-	let count = 0; 
-	const handleClick = (name) => {
-
-		if(count < 3){
-			count++;
-			console.log(`${name} ypu click me ${count}times `)
+	 let count = 0;
+	 const onClickHandler = (name) => {
+		if(count < 3 ){
+			count ++; 
+			console.log(`${name} you clicked me ${count} times`)
 		}
 		else {
-			console.log(`${name} stop clicking me !! `)
+			console.log(`${name} stop clicking me !! `);
 		}
-	}
+	 }
 
-
-	// event object = An object that contains information about the event that occured. It is passed as an argument to the event handler function.
-	const handleClick1 = (e) => {
-			console.log(e);
-	}
-
-
-	return( <button onClick={(e) => {handleClick1(e)}} >Click me</button> )
-
+	return ( <button onClick = {() => {onClickHandler('navneet')}}> Click me everday </button> )
 }
-
-
-export default Button
+export default Button 
